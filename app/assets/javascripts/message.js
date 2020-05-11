07d1170c36a9f9d1ee7be73f1acdb39bea63175e
ks0112 <k.sugihara.12@gmail.com>
2020-05-11 22:50:56 +0900

    add

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,65 @@
+$(function(){
+    function buildHTML(message){
+      if ( message.image ) {
+        let html =
+        `<div class="message">
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        let html =
+        `<div class="message">
+            <div class="upper-message">
+              <div class="upper-message__user-name">
+                ${message.user_name}
+              </div>
+              <div class="upper-message__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="lower-message">
+              <p class="lower-message__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
+$('#new_message').on('submit', function(e){
+    e.preventDefault()
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+     .done(function(data){
+       let html = buildHTML(data);
+       $('.messages').append(html);
+       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+       $('#new_message')[0].reset();
+       $('.submit-btn').attr('disabled', false);
+     })
+     .fail(function(){
+       alert('メッセージ送信に失敗しました');
+     });
+})
+})
